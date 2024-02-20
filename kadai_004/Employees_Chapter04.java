77b5795f176985b447e79da779008644af67ceb2
z4xRo1d <150352012+z4xRo1d@users.noreply.github.com>
2024-02-20 09:22:47 +0900

    Add files via upload

---
+++ kadai_004/Employees_Chapter04.java
@@ -0,0 +1,47 @@
+package kadai_004;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Employees_Chapter04 {
+	public static void main(String[] args) {
+		
+		Connection con = null;
+		Statement statement = null;
+		
+		try {
+			con = DriverManager.getConnection(
+				"jdbc:mysql://localhost/challenge_java",
+				"root",
+				"rfUjKGN9051"
+				);
+			
+			System.out.println("データベース接続成功");
+			
+			statement = con.createStatement();
+			String sql = """
+						CREATE TABLE employees(
+						 id INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+						 name VARCHAR(60) NOT NULL,
+						 email VARCHAR(255) NOT NULL,
+						 age INT(11),
+						 address VARCHAR(255)
+						 );
+					""";
+			
+			int rowCnt = statement.executeUpdate(sql);
+			System.out.println("テーブルを作成:rowCnt=" + rowCnt );
+		} catch(SQLException e) {
+			System.out.println("エラー発生: " + e.getMessage());
+		} finally {
+			if( statement != null) {
+				try { statement.close(); } catch(SQLException ignore) {}
+			}
+			if( con != null) {
+				try { con.close(); } catch(SQLException ignore) {}
+			}
+		}
+	}
+}
